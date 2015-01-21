3487b3991cbe08b9ba2745c98bd75ab155e22115
Fande Kong <fdkong.jd@gmail.com>
2015-01-21 10:13:56 -0700

    A bash script to compile and run the stream, record performance in term of compute time. The script will antomatically test different block sizes on the runtime.

---
+++ run-time.sh
@@ -0,0 +1,8 @@
+for n in {0..11}; do
+    gcc -O3 -march=native -mno-avx -fopenmp stream.c -DSTREAM_BLOCK_SIZE=$((2**n)) -DSTREAM_NUM_BLOCKS=$(((65536*1024)/(2**n)))  -o stream 
+    OMP_NUM_THREADS=4 ./stream \
+      | grep '\(Copy\|Scale\|Add\|Triad\|Dot\)' \
+      | awk '{print $3}' \
+      | xargs echo fdkong `hostname -f` Intel Core i5  2.6 2 $((2**n))  0.256 3  76.3 25600 \
+      | sed 's/ /,/g'
+    done | tee >> results/fdkong-time.csv
