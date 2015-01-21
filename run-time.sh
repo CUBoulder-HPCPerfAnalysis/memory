c570ccb9e55a02c404a2e629ec2c9ca7b9a986cf
Fande Kong <fdkong.jd@gmail.com>
2015-01-21 12:30:13 -0700

    Merge 'intel core i5' to one field. 'intel-core-i5'

--- run-time.sh
+++ run-time.sh
@@ -3,6 +3,6 @@ for n in {0..11}; do
     OMP_NUM_THREADS=4 ./stream \
       | grep '\(Copy\|Scale\|Add\|Triad\|Dot\)' \
       | awk '{print $3}' \
-      | xargs echo fdkong `hostname -f` Intel Core i5  2.6 2 $((2**n))  0.256 3  76.3 25600 \
+      | xargs echo fdkong `hostname -f` Intel-Core-i5  2.6 2 $((2**n))  0.256 3  76.3 25600 \
       | sed 's/ /,/g'
     done | tee >> results/fdkong-time.csv
