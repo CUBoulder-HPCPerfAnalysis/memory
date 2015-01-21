f059a377144adf6ae0095df6cba99089a2736c56
Ryan Skinner <anomaly1452@gmail.com>
2015-01-21 16:19:48 -0700

    Deleted erroneous comment in .py script

--- stream-analyze_ryan.py
+++ stream-analyze_ryan.py
@@ -14,7 +14,6 @@ df = pandas.concat(pandas.read_csv(f, na_values='?').rename(columns=normalize_co
                    for f in glob.glob('results/ryan_Add_BlkCyc.csv'))
 print(df)
 
-# Does performance depend on CPU clock speed?
 ax = df.plot(x='Stream Skip Length', y='Best Rate MB/s', kind='scatter')
 fig = ax.get_figure();
 fig.savefig('results/ryan_Best_Rate_vs_Skip_Length.png');
